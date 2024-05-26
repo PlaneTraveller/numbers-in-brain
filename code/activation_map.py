1b55965ad760678885d06a48b85fec668d5fbddf
Huayu Wang <why123jj@126.com>
2024-05-26 19:38:20 +0800

    feat: finish activation maps

--- code/activation_map.py
+++ code/activation_map.py
@@ -46,6 +46,7 @@ from subject_glm import betas_ind, p_ind
 from subject_glm import subj_p_ind
 from classifier import load_subj_glm
 from classifier import load_data
+from helpers import get_dd
 
 # from nltools.stats import one_sample_ttest
 # from statsmodels.stats.multitest import multipletests
@@ -54,6 +55,55 @@ from classifier import load_data
 def extract_betas_by_label(data, label):
     mask = data.Y == label
     return data[mask]
+
+
+def number_ttest(
+    data,
+    group_desc,
+    number,
+    p_value=0.05,
+    th_param={"fdr": 0.05},
+    results_dir="../results",
+):
+    extracted = extract_betas_by_label(data, number)
+    th_method = list(th_param.keys())[0]
+    th_value = th_param[th_method]
+
+    res = extracted.ttest(threshold_dict=th_param)
+    # print(res)
+    # p = res["p"]
+    # p.plot(view="mni", colorbar=True, threshold_upper=0, threshold_lower=p_value)
+    # p.plot(view="mni", colorbar=True)
+
+    thr_t = res["thr_t"]
+    thr_t.plot(view="mni", colorbar=True)
+    plt.savefig(
+        os.path.join(
+            results_dir,
+            "activation",
+            # f"{group_desc}_p{p_value}_{th_method}_{th_value}_{number}_ttest.png",
+            f"{group_desc}_{th_method}_{th_value}_{number}_ttest.png",
+        )
+    )
+    plt.close()
+
+
+def activation_map(
+    l, group, p_value=0.05, th_param={"fdr": 0.05}, results_dir="../results"
+):
+    numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    data = load_data(l, group)
+    group_desc = group["desc"]
+    number_ttest_curried = ft.partial(
+        number_ttest,
+        data,
+        group_desc,
+        p_value=p_value,
+        th_param=th_param,
+        results_dir=results_dir,
+    )
+
+    list(map(number_ttest_curried, numbers))
 
 
 # =============================================================
@@ -75,11 +125,18 @@ if __name__ == "__main__":
     toy10_group = {"desc": "toy-10", "subjs": subjs[:10]}
     full_group = {"desc": "full", "subjs": subjs}
 
-    data = load_data(l, toy_group)
+    dd_ta = get_dd(full_group)
+    dd_group = {"desc": "DD_full", "subjs": dd_ta["DD"]}
+    ta_group = {"desc": "TA_full", "subjs": dd_ta["TA"]}
 
-    beta_3 = extract_betas_by_label(data, "3")
+    # # Uncorrected: threshold for any voxel as we are not controlling for multiple tests
+    # activation_map(l, full_group, th_param={"unc": 0.001})
+    activation_map(l, dd_group, th_param={"unc": 0.01})
+    activation_map(l, ta_group, th_param={"unc": 0.01})
 
-    res = beta_3.ttest()
-    p = res["p"]
-    p.plot(view="mni", colorbar=True, threshold_upper=0, threshold_lower=0.05)
-    plt.savefig(os.path.join(results_dir, "activation", "toy_group_3_ttest.png"))
+    # # FDR corrected
+    # activation_map(l, full_group, th_param={"fdr": 0.05})
+
+    # activation_map(l, full_group, th_param={"unc": 0.005})
+    # activation_map(l, full_group, th_param={"unc": 0.01})
+    # activation_map(l, full_group, th_param={"fdr": 0.1})
